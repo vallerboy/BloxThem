a6272e826045bae9f4891b9d2194a38e5fe05255
Oskar Polak <oskar.polak@akademiakodu.pl>
2018-06-17 16:55:18 +0200

    ff

--- src/main/java/pl/oskarpolak/blox/models/services/UserService.java
+++ src/main/java/pl/oskarpolak/blox/models/services/UserService.java
@@ -1,6 +1,8 @@
 package pl.oskarpolak.blox.models.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 import pl.oskarpolak.blox.models.UserEntity;
 import pl.oskarpolak.blox.models.forms.RegisterForm;
@@ -9,10 +11,14 @@ import pl.oskarpolak.blox.models.repositories.UserRepository;
 import java.util.Optional;
 
 @Service
+@Scope(proxyMode = ScopedProxyMode.TARGET_CLASS, value = "session")
 public class UserService {
 
     @Autowired
     UserRepository userRepository;
+
+    private UserEntity userData;
+    private boolean isLogin;
 
     public boolean isUserExistByLogin(String login){
         return userRepository.existsByLogin(login);
@@ -33,7 +39,18 @@ public class UserService {
         Optional<UserEntity> loggedUser =
                 userRepository.findByLoginAndPassword(login, password);
 
-        //tutaj logika uwietrzlnienia
+        if(loggedUser.isPresent()){
+            isLogin = true;
+            userData = loggedUser.get();
+        }
         return loggedUser.isPresent();
+    }
+
+    public UserEntity getUserData() {
+        return userData;
+    }
+
+    public boolean isLogin() {
+        return isLogin;
     }
 }
