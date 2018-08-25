505b13998088db61fb354eab58124c99607f7043
Oskix <oskar.polak@akademiakodu.pl>
2018-08-25 10:29:00 +0200

    added some text to post html file

--- src/main/java/pl/oskarpolak/blox/models/repositories/CommentRepository.java
+++ src/main/java/pl/oskarpolak/blox/models/repositories/CommentRepository.java
@@ -1,9 +1,13 @@
 package pl.oskarpolak.blox.models.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.oskarpolak.blox.models.CommentEntity;
 
 @Repository
 public interface CommentRepository extends CrudRepository<CommentEntity, Integer> {
+
+    @Query(name = "UPDATE comment SET cos = ?1", nativeQuery = true)
+    void udapteCosTam(String cos);
 }
